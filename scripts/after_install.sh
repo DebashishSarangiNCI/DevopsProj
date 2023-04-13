d8bad951a9696d8aa5d692f90a0d60858303f46d
Ubuntu <ubuntu@ip-172-31-16-153.eu-west-1.compute.internal>
2023-04-13 17:07:54 +0000

    updated

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,18 +1,19 @@
-ll any servers that may be running in the background 
+#!/usr/bin/env bash
+
+#kill any servers that may be running in the background
 sudo pkill -f runserver
 
-# kill frontend servers if you are deploying any frontend
-# sudo pkill -f tailwind
-# sudo pkill -f node
+#kill frontend servers if you are deploying any frontend
+sudo pkill -f tailwind
+sudo pkill -f node
+cd /home/ec2-user/DevopsProj
 
-cd /home/ubuntu/django-aws_cicd/
-
-# activate virtual environment
+#activate virtual environment
 python3 -m venv env
 source env/bin/activate
 
-install requirements.txt
-pip install -r /home/ubuntu/environment/ls/DevopsProj/requirements.txt
+#install requirements.txt
+pip install -r /home/ec2-user/DevopsProj/requirements.txt
 
-# run server
+#run server
 screen -d -m python3 manage.py runserver 0:8000
