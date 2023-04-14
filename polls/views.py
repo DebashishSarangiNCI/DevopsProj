ed9ab56a549649021c912da4d4f29037f1071140
EC2 Default User <ec2-user@ip-172-31-12-197.eu-west-1.compute.internal>
2023-04-14 13:46:15 +0000

    upgrade

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from django.core.paginator import Paginator
 from django.db.models import Count
 from django.contrib import messages
 from .models import Poll, Choice, Vote
 from .forms import PollAddForm, EditPollForm, ChoiceAddForm
-from django.http import HttpResponse
+
 
 
 @login_required()
