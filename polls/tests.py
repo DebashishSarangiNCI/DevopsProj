5715934a559f4e606dbea7b2095f9e87acbf58a7
EC2 Default User <ec2-user@ip-172-31-12-197.eu-west-1.compute.internal>
2023-04-17 11:44:42 +0000

    upgrade

--- polls/tests.py
+++ polls/tests.py
@@ -1,11 +1,8 @@
 from django.test import TestCase
 import datetime
-
 from django.utils import timezone
 from django.urls import reverse
-
-from .models import Question
-
+from polls.models import Question
 
 class QuestionModelTest(TestCase):
     def test_was_published_recently_with_future_question(self):
