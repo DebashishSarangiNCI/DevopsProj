7d083a95c8fb148660653ef9f137874efa359752
EC2 Default User <ec2-user@ip-172-31-12-197.eu-west-1.compute.internal>
2023-04-17 12:06:02 +0000

    upgrade

--- polls/models.py
+++ polls/models.py
@@ -89,3 +89,10 @@ class Vote(models.Model):
 
     def __str__(self):
         return f'{self.poll.text[:15]} - {self.choice.choice_text[:15]} - {self.user.username}'
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
